39fed673d3df701e74b2221651794fdc835dc0b3
Olumide Josiah Akinremi <37152066+oluminous@users.noreply.github.com>
2019-04-07 17:57:45 +0100

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -7,4 +7,4 @@ default_model = 'default_model.h5'
 root_dir = Path(__file__).parents[1] # The root directory (mlclassification)
 model_dir = os.path.join(root_dir, "models") # the models directory
 
-model_extension = "h5"
+model_extension = ".h5"
