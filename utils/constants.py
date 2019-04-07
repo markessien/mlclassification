5409e69c420108b036bfb05f53171855ce248c64
EphraimBuddy <splendidzigy24@gmail.com>
2019-04-07 15:48:19 +0100

    remove some things from constants file

--- utils/constants.py
+++ utils/constants.py
@@ -2,6 +2,5 @@ import os
 from pathlib import Path
 
 model_default = 'default_model.h5'
-root_dir = Path(__file__).parents[1] # The root directory (mlclassification)
-model_dir = os.path.join(root_dir, "models") # the models directory
+
 model_extension = "h5"
