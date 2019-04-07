40768b1ffc2e77278c1e74f54cc78ef033bf78ad
Olumide Akinremi <josiahakinremi@gmail.com>
2019-04-07 10:58:29 +0100

    Renamed model to models and added functions in predict_model.py

--- utils/predict_model.py
+++ utils/predict_model.py
@@ -1,17 +1,24 @@
+
+
 # Importing the Keras libraries and packages
-import numpy as np
+
+import os
 import keras
+import numpy as np
 import tensorflow as tf
-from keras.models import load_model
-from IPython.display import display
+
+from pathlib import Path
 from PIL import Image
+
 from keras.preprocessing import image
 from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential
-from keras.layers import Conv2D
-from keras.layers import MaxPooling2D
-from keras.layers import Flatten
-from keras.layers import Dense
+
+from keras.models import Sequential, load_model
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+
+
+root_dir = Path(__file__).parents[1] # The root directory (mlclassification)
+model_dir = os.path.join(root_dir, "models") # the models directory
 
 
 train_datagen = ImageDataGenerator(
@@ -24,14 +31,27 @@ train_datagen = ImageDataGenerator(
 test_datagen = ImageDataGenerator(rescale=1./255)
 
 
-def import_model():
-    classifier = load_model("./model/best_weight.h5")
+def all_models():
+
+    all_models = [] # List of all the models in the models directory
+
+    for folder_name, folders, files in os.walk(model_dir):
+        for file in files:
+            if file.split('.')[1].lower() == 'h5':
+                all_models.append(file)
+
+    return all_models
+
+
+def import_model(model_name):
+
+    classifier = load_model(model_name)
+
     return classifier
 
 
-
 def test(classifier, test_img):
-    print("testing")
+
     test_image = prepImage(test_img)
     result = classifier.predict(test_image)
     return printResult(result)
