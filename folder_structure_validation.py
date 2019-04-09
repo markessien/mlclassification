fa752f709c3cee7ccce4232f57a0d99b2ee6d8ba
munnyraht <muniratsulaimon@gmail.com>
2019-04-09 17:06:18 +0100

    munirat, folder structure validation

--- folder_structure_validation.py
+++ folder_structure_validation.py
@@ -6,78 +6,64 @@ class validate_dataset:
     def check_two_folders(self,path):
         import os
         folderdir=[]
-        foldername=[]
         files=os.listdir(path)
         for file in files:
             file=os.path.join(path,file)
             if os.path.isfile(file)==False:
                 folderdir.append(file)
-                foldername.append(os.path.split(file)[1])
         if len(folderdir)!=2:
             return False
         else:
-            return folderdir, foldername
+            return folderdir
         
     def validate(self):
         import os
         import json
         #check if path is an existing path
-        if os.path.isdir(path)=='False':
+        if os.path.isdir(path)==False:
             result=json.dumps({'error':'true','message':'traning path provided not found'})
             return result
         else: 
             #check if path is a folder or file
-            if os.path.isfile(path)=='True':
+            if os.path.isfile(path)==True:
                 result=json.dumps({'error':'true','message':'invalid training path provided'})
                 return result
             else:
                 #check if dir is not empty
                 if os.listdir(path)==[]:
-                    result=json.dumps({'error':'false','message':'emptypath specified'})
+                    result=json.dumps({'error':'true','message':'emptypath specified'})
                     return result
-
                 else:
-                    #check contents of the file
+                    #check if the path contains two folders
                     check1=self.check_two_folders(path)
                     if (check1 == False):
-                        result=json.dumps({'error':'false','message':'folder contained in path is not equal to 2'})
+                        result=json.dumps({'error':'true','message':'folder must contain 2 sub-folders'})
                         return result
-                        
+                    
                     else:
-                        #check the names of the folder
-                        folderdir,foldername=check1
-                        if ((foldername[0]!='test_set') and (foldername[1]!='training_set')):
-                            result=json.dumps({'error':'false','message':'folder not named correctly, pls name folders -test_set- and -traning_set- simultaneously'})
-                            return result
-                            
-                        else:
-                            for folder in folderdir:              
-                                #check if each folder contains 2 folder each
-                                check2=self.check_two_folders(folder)
-                                if (check2 == False):
-                                    result=json.dumps({'error':'false','message':'either folder does not contain 2 subfolders'})
-                                    return result
+                        #check if the contents of the folders are images
+                        image_ext=['.jpeg','.jpg','.png','.gif','.tiff']
+                        image=[]
+                        for i, subs in enumerate (check1):
+                            files= os.listdir(subs)
+                            for file in files:
+                                file_extention=os.path.splitext(file)[1].lower()
+                                #print('file_ext',file_extention)
+                                if file_extention in image_ext:
+                                    image.append(True)
                                 else:
-                                    folderdir,filename=check2
-                                #check if the contents of the folders are images
-                                image_ext=['.jpeg','.jpg','.png','.gif']
-                                image=[]
-                                for subs in folderdir:
-                                    files= os.listdir(subs)
-                                    for file in files:
-                                        file_extention=os.path.splitext(file)[1].lower()
-                                        #print('file_ext',file_extention)
-                                        if file_extention in image_ext:
-                                            image.append(True)
-                                        else:
-                                            image.append(False)
-                            if False in image:
-                                
-                                result=json.dumps({'error':'false','message':'either sub-folder contains a non-image file'})
-                                return result
-                            else:
-                                result=json.dumps({'error':'false','message':'Validation Completed'})
-                                return result
+                                    image.append(False)
+                        if False in image:
+                            result=json.dumps({'error':'true','message':f' either sub-folder contains a non-image file'})
+                            return result
+                        else:
+                            result=json.dumps({'error':'false','message':'Validation Completed'})
+                            return result
+
+
+#how to call method
+#create an instance of  the class e.g x= validate_dataset(path)
+#then call the function validate e.g x.validate()
                                         
                                    
                                 
