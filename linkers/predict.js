1e7dd055dd5bf75307085abfbcca975d64badcda
Akinduko Olugbenga <oakinduko@MACs-MacBook-Pro-3.local>
2019-04-12 08:18:24 +0100

    feat: pushed current implementation

--- linkers/predict.js
+++ linkers/predict.js
@@ -1,16 +1,30 @@
+var {
+    PythonShell
+} = require("python-shell");
+var path = require('path');
 class Store {
     constructor() {
-        this.store = {}
+        this.groups = {}
+        this.predict = {}
     }
-    addItem(name, path) {
-        this.store[name] = path;
+    addGroup(name, path) {
+        this.groups[name] = path;
     }
-
-    getItem() {
-        return this.store;
+    addPredict(path) {
+        this.predict.path = path;
+    }
+    getGroup() {
+        return this.groups;
+    }
+    getPredict() {
+        return this.predict;
+    }
+    clearGreat() {
+        this.groups = {};
     }
 }
 
+const store = new Store();
 var trainingFolders = new Object();
 
 const predictImage = () => {
@@ -25,13 +39,49 @@ const predictImage = () => {
 }
 
 function addGroup(event, clast) {
-    // var trainingFolders = new Object();
-    // const files = event.target.files;
-    // var folder;
-    // folder = files[0];
-    // folder = folder.webkitRelativePath;
+    const files = event.target.files;
+    var folder = files[0];
+    folder = folder.webkitRelativePath;
+    store.addGroup(clast, folder)
+    document.getElementById(`visible${clast}`).placeholder = folder;
+    document.getElementById(`trainbutton`).disabled = false;
     console.log('Saving Directory')
-    console.log(store.getItem());
+    Swal.fire({
+        html: `You can use <b>${JSON.stringify(store.getGroup())}</b>`,
+        showCloseButton: true,
+        showCancelButton: true,
+        focusConfirm: false,
+        confirmButtonText: 'Proceed',
+        preConfirm: () => {
+            return [
+                train()
+            ]
+        },
+        cancelButtonText: '<i class="fa fa-thumbs-down"></i> Cancel',
+    })
+    console.log('Saved Directory')
+}
+
+function addPredict(event) {
+    const files = event.target.files;
+    var folder = files[0];
+    folder = folder.webkitRelativePath;
+    store.addPredict(folder)
+    document.getElementById(`predictinput`).placeholder = folder;
+    // document.getElementById(`predictbutton`).disabled = false;
+    Swal.fire({
+        html: `You can use <b>${ path.join(__dirname, folder)}</b>`,
+        showCloseButton: true,
+        showCancelButton: true,
+        focusConfirm: false,
+        confirmButtonText: 'Proceed',
+        preConfirm: () => {
+            return [
+                predict()
+            ]
+        },
+        cancelButtonText: '<i class="fa fa-thumbs-down"></i> Cancel',
+    })
     console.log('Saved Directory')
 }
 
@@ -40,24 +90,59 @@ function train() {
 }
 
 function predict() {
-    var {
-        PythonShell
-    } = require("python-shell");
-    var path = require('path');
+    console.log('hello')
+    document.getElementById('predictprogress').value = 30
     var options = {
         scriptPath: path.join(__dirname, '/../server'),
         pythonPath: '/usr/local/bin/python3',
-        args : ['predict', '-path', '../testimages/nh3.jpg']
+        args: ['predict', '-path', 'testimages/nh3.jpg']
     }
-
     try {
+
         const predict = new PythonShell('app.py', options);
         predict.end((err, code, message) => {
-            console.log(err, code, message)
+            const perc = 100;
+            document.getElementById('predictprogress').value = perc;
+            document.getElementById('predicttag').innerText = `${perc}%`;
+            document.getElementById('predictresult').innerText = 'Completed';
+            document.getElementById(`slides`).hidden = false;
+            const slides = document.getElementById(`sliders`);
+            for (let i = 3; i < 8; i++) {
+                const img = document.createElement("img");
+                img.src = `./testimages/nh${i}.jpg`;
+                img.className = "mySlides"
+                img.style = "width:100%; height:250px;";
+                slides.appendChild(img);
+            }
+            showDivs(5)
         })
     } catch (error) {
         console.log(error)
     }
 }
 
-predict()
+function showDivs(n) {
+    let i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = x.length
+    }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
+    var y = document.getElementById("confidencetext");
+    updateConfidenceProgress("confidence", 'progress--100')
+    y.innerText = '100%'
+
+}
+
+function updateConfidenceProgress(id, name) {
+    var element, name, arr;
+    element = document.getElementById(id);
+    arr = element.className.split(" ");
+    element.className = arr[0] + " " + name;
+}
