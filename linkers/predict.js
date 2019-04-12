6c2714f1d7a41975c1f105f031e02891c442eace
Akinduko Olugbenga <oakinduko@MACs-MacBook-Pro-3.local>
2019-04-12 03:48:22 +0100

    feat: modified internal files

--- linkers/predict.js
+++ linkers/predict.js
@@ -1,13 +1,19 @@
-const {
-    PythonShell
-} = require("python-shell");
-const path = require('path');
-const options = {
-    scriptPath: path.join(__dirname, '/../server'),
-    pythonPath: '/usr/local/bin/python3'
-}
-const predictImage = () => {
+class Store {
+    constructor() {
+        this.store = {}
+    }
+    addItem(name, path) {
+        this.store[name] = path;
+    }
 
+    getItem() {
+        return this.store;
+    }
+}
+
+var trainingFolders = new Object();
+
+const predictImage = () => {
     try {
         const predict = new PythonShell('app.py train', options);
         predict.end((err, code, message) => {
@@ -16,20 +22,42 @@ const predictImage = () => {
     } catch (error) {
         console.log(error)
     }
-
-
 }
 
-function addGroup(input) {
-    console.log(input, document.getElementById('files').value);
-
+function addGroup(event, clast) {
+    // var trainingFolders = new Object();
+    // const files = event.target.files;
+    // var folder;
+    // folder = files[0];
+    // folder = folder.webkitRelativePath;
+    console.log('Saving Directory')
+    console.log(store.getItem());
+    console.log('Saved Directory')
 }
-
 
 function train() {
     console.log('train');
 }
 
 function predict() {
-    console.log('predict');
+    var {
+        PythonShell
+    } = require("python-shell");
+    var path = require('path');
+    var options = {
+        scriptPath: path.join(__dirname, '/../server'),
+        pythonPath: '/usr/local/bin/python3',
+        args : ['predict', '-path', '../testimages/nh3.jpg']
+    }
+
+    try {
+        const predict = new PythonShell('app.py', options);
+        predict.end((err, code, message) => {
+            console.log(err, code, message)
+        })
+    } catch (error) {
+        console.log(error)
+    }
 }
+
+predict()
